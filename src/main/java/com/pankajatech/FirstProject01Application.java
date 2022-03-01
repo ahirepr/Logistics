fd723c97d6eb890da65591b78b51ca1de7a6b6b5
pahire <ahirepr2017@gmail.com>
2022-03-01 22:53:28 +0530

    Initial commit by TL

---
+++ src/main/java/com/pankajatech/FirstProject01Application.java
@@ -0,0 +1,13 @@
+package com.pankajatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProject01Application.class, args);
+	}
+
+}
